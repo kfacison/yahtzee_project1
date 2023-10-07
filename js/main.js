5c0f59a7fe392bb38459d9b42a4f5aa21fd0add5
kfacison <kfacison1998@gmail.com>
2023-10-07 15:35:07 -0500

    added reroll function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 /*----- constants -----*/
 class Dice{
     value = 0
-roll = function(){
-this.value = Math.floor(Math.random() * 6) + 1;
+    roll = function(){this.value = Math.floor(Math.random() * 6) + 1;}
 }
-}
+const dice1 = new Dice;
+const dice2 = new Dice;
+const dice3 = new Dice;
+const dice4 = new Dice;
+const dice5 = new Dice;
+const cupOfDice =[dice1,dice2,dice3,dice4,dice5];
+
 
 
 /*----- state variables -----*/
@@ -18,3 +23,12 @@ this.value = Math.floor(Math.random() * 6) + 1;
 
 /*----- functions -----*/
 
+function rollAllDice(){
+    cupOfDice.forEach(dice => {dice.roll()});
+}
+
+//pass index of cupOfDice to reroll
+function reroll(...diceToReroll){
+    diceToReroll.forEach(diceIdx => {cupOfDice[diceIdx].roll();});
+}
+console.log(cupOfDice);
